05cb0a814092ecac74696e17a7a87c324a38270c
Davide Marzona <davidemarzona@gmail.com>
2024-08-04 00:11:06 +0200

    Fixed bug when difference between two times is done, introduced time to activate sprinkler
    Doing GetMinutes() + 5 was introducing a bug when the hour was changing.
    The same would occur when at the end of the month there should be the
    update of the our.
    
    Added two activation times for sprinkler

--- Application.ino
+++ Application.ino
@@ -4,9 +4,14 @@
 Adafruit_BMP280 bmp;
 extern CTime current_epoch_time;
 CTime application_time;
+CTime first_activation;
+CTime second_activation;
 
 void mainApplication(void* parameter)
 {
+    first_activation.UpdateEpoch("1970-01-01 06:00:00");
+    second_activation.UpdateEpoch("1970-01-01 21:00:00");
+
     if (!bmp.begin(0x76))
     {
         SendSerialMessage("Sensor not found\n");
@@ -26,7 +31,7 @@ void mainApplication(void* parameter)
     while(true)
     {
         // log only every five minutes
-        if(current_epoch_time.GetMinutes() >= application_time.GetMinutes() + 5)
+        if ((current_epoch_time - application_time) >= 5*MINUTE_IN_SECONDS)
         {
             if (bmp.takeForcedMeasurement())
             {
--- TimeManager.ino
+++ TimeManager.ino
@@ -9,18 +9,18 @@ void updateTime(void* parameter)
   timeClient.update();
 
   current_epoch_time.UpdateEpoch(timeClient.getEpochTime());
-  last_saved_epoch_time.UpdateEpoch(current_epoch_time.GetEpoch());
+  last_saved_epoch_time = current_epoch_time;
   
   while(true)
   {
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     current_epoch_time.Increase();
-    if (current_epoch_time.GetDay() >= last_saved_epoch_time.GetDay() + 1)
+    if ((current_epoch_time - last_saved_epoch_time) >= DAY_IN_SECONDS)
     {
       timeClient.update();
       time_t temp_epoch_time = current_epoch_time.GetEpoch();
       current_epoch_time.UpdateEpoch(timeClient.getEpochTime());
-      last_saved_epoch_time.UpdateEpoch(current_epoch_time.GetEpoch());
+      last_saved_epoch_time = current_epoch_time;
       int error = abs(int((signed long)(current_epoch_time.GetEpoch()) - (signed long)(temp_epoch_time)));
       log("Current time updated, difference %d", error);
     }    
