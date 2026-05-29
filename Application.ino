dff67889ea98af4aafe32b7149ac15f2f01883b3
Davide Marzona <davidemarzona@gmail.com>
2026-05-29 22:37:46 +0200

    Initialized new pump and humidity sensor
    Used serial instead of log. For the moment, pump is used only in one direction

--- Application.ino
+++ Application.ino
@@ -1,64 +1,68 @@
-#include <Adafruit_BMP280.h>
+#include <Wire.h>
+#include <SHT40.h>
 #include "src/CTime.h"
 #include "src/WebsiteVariables.h"
+#include "src/CPump.h"
 
-Adafruit_BMP280 bmp;
+// I2C pins for humidity sensor
+#define SDA_PIN 8
+#define SCL_PIN 18
+
+// Bridge pin definitions
+#define PWM1  9
+#define PWM2 48
+#define EN1  46
+#define EN2  47
+#define CS1   3
+#define CS2  14
+
+SHT40 sht40;
 extern CTime current_epoch_time;
 extern WebsiteVariables variables;
 extern CFlash application_information;
 CTime application_time;
 CTime pump_activation_time;
+CPump pump(PWM1, EN1, CS1, PWM2, EN2, CS2);
 
 bool pump_override = false;
 
-float getCurrentSensor()
+bool setHumiditySensor()
 {
-    int current_sensor_read = analogRead(currentSensorPin);
-    float voltage_on_current_sensor = 3.3*(float)(current_sensor_read)/4096.0;
-    return voltage_on_current_sensor;
+    Wire.begin(SDA_PIN, SCL_PIN);
+    return sht40.begin(&Wire);
 }
 
 void mainApplication(void* parameter)
 {
     static int pump_start = false;
 
-    if (!bmp.begin(0x76))
+    if (!setHumiditySensor())
     {
         SendSerialMessage("Sensor not found\n");
     }
     else
     {
-        bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
-                        Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                        Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                        Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                        Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
         SendSerialMessage("Sensor properly set\n");
     }
 
     application_time = current_epoch_time;
 
-    if (bmp.takeForcedMeasurement())
-    {
-        variables.UpdateTemperature(bmp.readTemperature());
-        variables.UpdatePressure(bmp.readPressure());
-        variables.UpdateCurrentSense(getCurrentSensor());
-    }
+    variables.UpdateTemperature(sht40.readTemperatureC());
+    variables.UpdatePressure(sht40.readHumidityRH());
+    variables.UpdateCurrentSense(pump.getCurrent());
 
     while(true)
     {   
         // log only every five minutes
         if ((current_epoch_time - application_time) >= 5*MINUTE_IN_SECONDS)
         {
-            if (bmp.takeForcedMeasurement())
-            {
-                variables.UpdateTemperature(bmp.readTemperature());
-                variables.UpdatePressure(bmp.readPressure());
-                log("Temperature: %.2f *C, Pressure: %.2f Pa", variables.GetTemperature(), variables.GetPressure());
-            }
 
-            variables.UpdateCurrentSense(getCurrentSensor());
-            log("Voltage at current sensor interface: %.2f V", variables.GetCurrentSense());
+            variables.UpdateTemperature(sht40.readTemperatureC());
+            variables.UpdatePressure(sht40.readHumidityRH());
+            SendSerialMessage("Temperature: %.2f *C, RH: %.2f %\n", variables.GetTemperature(), variables.GetPressure());
+
+            variables.UpdateCurrentSense(pump.getCurrent());
+            SendSerialMessage("Voltage at current sensor interface: %.2f V\n", variables.GetCurrentSense());
             // store time
             application_time = current_epoch_time;
         }
@@ -78,8 +82,9 @@ void mainApplication(void* parameter)
                 pump_start = true;
                 pump_override = false;
                 pump_activation_time = current_epoch_time;
-                analogWrite(pumpPin, application_information.GetPumpPowerRaw());
-                log("Pump active");
+                pump.setPwmDirection1(application_information.GetPumpPowerRaw());
+                pump.activatePumpDirection1(true);
+                SendSerialMessage("Pump active\n");
                 break;
             }
         }
@@ -87,14 +92,14 @@ void mainApplication(void* parameter)
         if (((current_epoch_time - pump_activation_time) >= application_information.GetActivationTime()) && pump_start)
         {
             pump_start = false;
-            analogWrite(pumpPin, 0);
-            log("Pump turn-off");
+            pump.activatePumpDirection1(false);
+            SendSerialMessage("Pump turn-off\n");
         }
 
         if(pump_start)
         {
-            variables.UpdateCurrentSense(getCurrentSensor());
-            log("Voltage at current sensor interface: %.2f V", variables.GetCurrentSense());
+            variables.UpdateCurrentSense(pump.getCurrent());
+            SendSerialMessage("Voltage at current sensor interface: %.2f V\n", variables.GetCurrentSense());
         }
 
         vTaskDelay(1000 / portTICK_PERIOD_MS);
